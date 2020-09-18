286a629a919d8c009de9cd72c19cb5affc3cb837
vinceresss <Gitvinc11>
2020-09-19 01:32:41 +0300

    adding files

--- src/ru/mirea/ivbo09/Main.java
+++ src/ru/mirea/ivbo09/Main.java
@@ -1,22 +1,21 @@
 package ru.mirea.ivbo09;
+
 import java.util.Scanner;
+import java.util.Formatter;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        int task=scanner.nextInt();
-        switch(task){
+        Scanner scanner = new Scanner(System.in);
+        int task = scanner.nextInt();
+        switch (task) {
             case 1:
-                int Array[]={23, 5, 12, 33, 9, 10, 11, 6};
-                int value=0;
+                int Array[] = {23, 5, 12, 33, 9, 10, 11, 6};
+                int value = 0;
                 for (int i = 0; i < Array.length; i++) {
-                    value+=Array[i];
+                    value += Array[i];
                 }
                 System.out.println("Value = " + value);
-            case 2:
-
-
+            case 3:
         }
-
     }
 }

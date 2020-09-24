f9181d095513dbf66484b74603312b3b3eb8c025
vinceresss <Gitvinc11>
2020-09-24 23:01:53 +0300

    adding files

--- src/ru/mirea/ivbo09/Main.java
+++ src/ru/mirea/ivbo09/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 
 
 public class Main {
+    static int Factorial(int fact) {
+        if (fact == 1)
+            return 1;
+        return (Factorial(fact - 1) * fact);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int task = scanner.nextInt();
@@ -34,9 +40,9 @@ public class Main {
                     System.out.print("  " + Array2[i]);
                 }
                 System.out.println();
-                boolean sorted=false;
+                boolean sorted = false;
                 int temp;
-                while(!sorted) {
+                while (!sorted) {
                     sorted = true;
                     for (int i = 0; i < Array2.length - 1; i++) {
                         if (Array2[i] > Array2[i + 1]) {
@@ -49,20 +55,20 @@ public class Main {
                     }
                 }
                 for (int i = 0; i < 10; i++) {
-                    System.out.print("  "+Array2[i]);
+                    System.out.print("  " + Array2[i]);
                 }
-                    System.out.println();
-                    int Array3[] = new int[10];
-                    Random random = new Random();
-                    for (int a = 0; a < 10; a++) {
-                        int b = random.nextInt(100) + 1;
-                        Array3[a] = b;
-                        System.out.print("  " + Array3[a]);
-                    }
                 System.out.println();
-                boolean sort=false;
+                int Array3[] = new int[10];
+                Random random = new Random();
+                for (int a = 0; a < 10; a++) {
+                    int b = random.nextInt(100) + 1;
+                    Array3[a] = b;
+                    System.out.print("  " + Array3[a]);
+                }
+                System.out.println();
+                boolean sort = false;
                 int temp2;
-                while(!sort) {
+                while (!sort) {
                     sort = true;
                     for (int i = 0; i < Array3.length - 1; i++) {
                         if (Array3[i] > Array3[i + 1]) {
@@ -75,10 +81,18 @@ public class Main {
                     }
                 }
                 for (int i = 0; i < 10; i++) {
-                    System.out.print("  "+Array3[i]);
+                    System.out.print("  " + Array3[i]);
                 }
-
-
+            case 7:
+                int number = scanner.nextInt();
+                int number2=number;
+                number=Factorial(number);
+                System.out.println("Факториал методом рекурсии = " +number);
+                int result = 1;
+                for (int i = 1; i <=number2; i ++){
+                    result = result*i;
+                }
+                System.out.print("Проверка = "+result);
 
         }
     }

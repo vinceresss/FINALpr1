9459ec91980d913aa507dfe064e0f53827659a6a
vinceresss <Gitvinc11>
2020-09-24 22:02:10 +0300

    adding files

--- src/ru/mirea/ivbo09/Main.java
+++ src/ru/mirea/ivbo09/Main.java
@@ -1,5 +1,6 @@
 package ru.mirea.ivbo09;
 
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -16,15 +17,30 @@ public class Main {
                 }
                 System.out.println("Value = " + value);
             case 4:
-                for(String str: args){
+                for (String str : args) {
                     System.out.println(args);
                 }
             case 5:
                 for (double i = 1; i <= 10; i++) {
-                    System.out.printf("%5.2f", 1/i);
+                    System.out.printf("%5.2f", 1 / i);
                 }
 
             case 6:
+                int Array2[] = new int[10];
+                int d;
+                for (int i = 0; i < 10; i++) {
+                    d=(int)(Math.random()*20+1);
+                    Array2[i]=d;
+                    System.out.print("  "+Array2[i]);
+                }
+                System.out.println();
+                int Array3[]=new int[10];
+                Random random = new Random();
+                for (int a = 0; a < 10; a++) {
+                    int b = random.nextInt(20)+1;
+                    Array3[a]=b;
+                    System.out.print("  "+Array3[a]);
+                }
 
 
 
